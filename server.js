a9318383958b94e2bbbc8ec0df0ef6c857ad4666
陳河穆 <hmchen@myhost.local>
2018-10-19 13:25:02 +0800

    add plugin setLocaleCokie, enabling to pass "locale" query string, default en_US

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+  followRedirects: true,
+});
 
-server.use(prerender.sendPrerenderHeader());
+server.use(prerender.setLocaleCookie());
+//server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+//server.use(prerender.removeScriptTags());
+//server.use(prerender.httpHeaders());
 
 server.start();
